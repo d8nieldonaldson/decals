14984c6094f7920b6a66ba4eb813e639083cbc36
Daniel <daniel.donaldson@definitinon6.com>
2021-03-03 10:35:24 -0800

    first

---
+++ World/World.js
@@ -0,0 +1,47 @@
+import { createCamera } from './components/camera.js';
+import { createCube } from './components/cube.js';
+import { createLights } from './components/lights.js';
+import { createScene } from './components/scene.js';
+
+import { createRenderer } from './systems/renderer.js';
+import { Resizer } from './systems/Resizer.js';
+import { Loop } from './systems/Loop.js';
+
+let scene;
+let camera;
+let renderer;
+let loop;
+
+class World {
+    constructor(container) {
+        camera = createCamera();
+        scene = createScene();
+        renderer = createRenderer();
+        loop = new Loop(camera, scene, renderer);
+        container.append(renderer.domElement);
+
+        const cube = createCube();
+        const light = createLights();
+
+        scene.add(cube, light);
+
+        const resizer = new Resizer(container, camera, renderer);
+        resizer.onResize = () => {
+            this.render();
+        };
+    }
+
+    render() {
+        // draw a single frame
+        renderer.render(scene, camera);
+    }
+
+    start() {
+        loop.start();
+    }
+    stop() {
+        loop.stop();
+    }
+}
+
+export { World };
---
+++ main.js
@@ -0,0 +1,17 @@
+import { World } from './World/World.js';
+
+function main() {
+    // Get a reference to the container element
+    const container = document.querySelector('#scene-container');
+
+    // 1. Create an instance of the World app
+    const world = new World(container);
+
+    // // 2. Render the scene
+    // world.render();
+
+    // 2. start the loop
+    world.start();
+}
+
+main();
